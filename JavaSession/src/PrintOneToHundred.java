ec4747ecf4666f32a3f189a922837341bea11978
Lenovo <Lenovo@DESKTOP-JUM6TNF>
2021-09-12 11:35:32 +0530

    my first commit

---
+++ JavaSession/src/PrintOneToHundred.java
@@ -0,0 +1,17 @@
+
+public class PrintOneToHundred {
+
+	public static void main(String[] args) {
+		// print one to hundred without using any number
+		
+		int one='a'/'a';
+		String s1 ="..........";
+		for(int i=one;i<=s1.length() * s1.length();i++)
+		{
+			System.out.println(i);
+		}
+
+		
+	}
+
+}
